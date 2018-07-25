44a9d228e3804cefd1e9a2a231eeb916b3ee6628
Sauce <sauce@mavs.uta.edu>
2018-07-24 19:56:53 -0500

    robot able to move through goals

--- scripts/runugv.sh
+++ scripts/runugv.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-roslaunch ugv ugv.launch
+sleep 30
+source /opt/ros/kinetic/setup.bash
+source /home/smgrobot/catkin_ws/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.1.100:11311
+export ROS_HOSTNAME=192.168.1.100
+roslaunch ugv start.launch
